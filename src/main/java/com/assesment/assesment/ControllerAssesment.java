12f0023359497acdbcd6eba060e7c2705e094511
Rachana Sharma <45629361+Rachana-Sharma@users.noreply.github.com>
2022-04-09 04:37:06 +0530

    Change in mapping annotation

--- src/main/java/com/assesment/assesment/ControllerAssesment.java
+++ src/main/java/com/assesment/assesment/ControllerAssesment.java
@@ -16,20 +16,9 @@ public class ControllerAssesment {
 	 @Autowired
 	    private ServiceAssesment serviceAssesment;
 	 
-	/* @RequestMapping("/calculator")
-	    public String getCalculatorPage(Model model){
-	        model.addAttribute("calculatemodel",model);
-	        return "calculator";
-	    }
-	 
-	 @RequestMapping(value="/calculator", params="add", method = RequestMethod.POST)
-	    public String add(@ModelAttribute("calculatemodel")  CalculateModel calculatemodel, Model model ){
-	        model.addAttribute("result", serviceAssesment.add(calculatemodel));
-	        return "calculator";
-	    }*/
-	 @RequestMapping(value="/add", method=RequestMethod.POST)
-		public int calculateSum(@RequestBody CalculateModel calculatemodel) {
-			
-	return  serviceAssesment.add(calculatemodel);
-}
+	
+	 @PostMapping(value="/add")
+         public int calculateSum(@RequestBody CalculateModel calculatemodel) {			
+	 return  serviceAssesment.add(calculatemodel);
+	}
 }
